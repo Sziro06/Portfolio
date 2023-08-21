e01bab223afef6b32c230f218effd02e91c4eb09
Sziro06 <120630233+Sziro06@users.noreply.github.com>
2023-08-21 22:26:47 +0200

    Menu
    used nav element and made a menu that you can open and remove menu with buttons.

--- startscreen.js
+++ startscreen.js
@@ -1,5 +1,17 @@
+let navIsOpen = document.getElementById("navIsOpen");
+let obtn = document.getElementById("obtn");
+
 function changemode(){
     let changeBackground = document.body;
     changeBackground.classList.toggle("dark-mode");
 }
 
+function openNav(){
+    navIsOpen.style.display = "block"
+    obtn.style.display = "none"
+}
+
+function lockNav(){
+    navIsOpen.style.display = "none"
+    obtn.style.display = "block"
+}
